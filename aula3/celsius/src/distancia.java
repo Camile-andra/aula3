c8fe53db27407409bfa5758d24181491d5f9b374
Camile <161327935+Camile-andra@users.noreply.github.com>
2024-03-04 22:23:45 -0300

    Update distancia.java

--- aula3/celsius/src/distancia.java
+++ aula3/celsius/src/distancia.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 public class distancia {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Digite o primeiro ponto: ");
-        double n1 = s.nextDouble();
-        System.out.print("Digite o segundo ponto: ");
-        double n2 = s.nextDouble();
-        double conta = n1 - n2;
+        
+        System.out.print("Digite o x do primeiro ponto: ");
+        double x1 = s.nextDouble();
+        System.out.print("Digite o y do segundo ponto: ");
+        double y1 = s.nextDouble();
+
+        System.out.print("Digite o x do primeiro ponto: ");
+        double x2 = s.nextDouble();
+        System.out.print("Digite o y do segundo ponto: ");
+        double y2 = s.nextDouble();
+        
+        double conta = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
         System.out.println(conta);
     }
